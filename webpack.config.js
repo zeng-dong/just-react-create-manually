054a3d1b59fe65324e1df8fc219a4ee00ea870c6
Dong Zeng <zeng-dong@hotmail.com>
2022-11-19 15:47:56 -0600

    update webpack to enable transpiling

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,20 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist')
     },
+    module: {
+        rules: [
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    },
     devServer: {
         open: true
     },
